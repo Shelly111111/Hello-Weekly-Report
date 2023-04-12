d9ff07c823d423a93ef374b6b7aa0a5336c751d8
戈 张 <921519274@qq.com>
2023-04-12 11:04:34 +0800

    Init Commit

---
+++ spring-Java/src/main/java/com/hello/weekly/WeeklyReportApplication.java
@@ -0,0 +1,13 @@
+package com.hello.weekly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeeklyReportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeeklyReportApplication.class, args);
+    }
+
+}
---
+++ spring-Java/src/test/java/com/hello/weekly/WeeklyReportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hello.weekly;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeeklyReportApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
