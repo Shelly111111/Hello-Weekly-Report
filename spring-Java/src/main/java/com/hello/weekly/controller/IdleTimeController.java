ff2e38752ad92c649da668211389d3168f9fd7ce
wang-teng0425 <11983104+wang-teng0425@user.noreply.gitee.com>
2023-04-17 18:56:32 +0800

    idletime

--- spring-Java/src/main/java/com/hello/weekly/controller/IdleTimeController.java
+++ spring-Java/src/main/java/com/hello/weekly/controller/IdleTimeController.java
@@ -1,9 +1,12 @@
 package com.hello.weekly.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.hello.weekly.Res.ResponseData;
 import com.hello.weekly.mapper.IdletimeMapper;
+import com.hello.weekly.mapper.UserMapper;
 import com.hello.weekly.pojo.IdleTime;
 import com.hello.weekly.pojo.User;
+import com.hello.weekly.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,12 +20,26 @@ public class IdleTimeController {
     @Autowired
     private IdletimeMapper idletimeMapper;
 
+
+    @Autowired
+    private UserService userService;
+
     @GetMapping("/userdaily/idletime")
-    public ResponseData findIdleTime(User user){
+    public ResponseData findIdleTime(String username){
 
-        //List<IdleTime> list = idletimeMapper.selectList(null);
 
-        return new ResponseData(ResponseData.success,"查询成功！",idletimeMapper.selectById(user.getId()));
+         int userid  = userService.findByUsername(username).getId();
+        //System.out.println(userid);
+        //QueryWrapper<IdleTime> wrapper = new QueryWrapper<IdleTime>();
+        //wrapper.eq("Id",userid);
+        if (userid>0){
+        IdleTime idleTime = idletimeMapper.selectById(userid);
+
+        return new ResponseData(ResponseData.success,"查询成功！",idleTime);
+        }else {
+            return new ResponseData(ResponseData.notFound,"未找到用户");
+        }
+
         //return new ResponseData(ResponseData.success,"",list);
     }
 
