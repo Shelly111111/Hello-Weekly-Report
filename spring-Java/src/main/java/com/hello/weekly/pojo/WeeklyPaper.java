51860b71d3892b19829466da0c84288d6f7be2ee
Admin <1139921410@qq.com>
2023-04-17 09:51:36 +0800

    update bug

--- spring-Java/src/main/java/com/hello/weekly/pojo/WeeklyPaper.java
+++ spring-Java/src/main/java/com/hello/weekly/pojo/WeeklyPaper.java
@@ -3,7 +3,6 @@ package com.hello.weekly.pojo;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.AllArgsConstructor;
-<<<<<<< HEAD
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
@@ -20,7 +19,7 @@ public class WeeklyPaper {
     private Integer Id;
 
     @TableField("userId")
-    private  Integer userId;
+    private Integer userId;
 
     @TableField("date")
     private String date;
@@ -33,33 +32,4 @@ public class WeeklyPaper {
 
     @TableField("delay")
     private String delay;
-=======
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-import java.util.Date;
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-@TableName("weeklypaper")
-public class WeeklyPaper {
-    private Integer Id;
-    //周报标识符
-    @TableField(value = "userid")
-    private Integer userId;
-    //用户Id
-    private Date date;
-    //日报日期
-    @TableField(value = "completework")
-    private String completeWork;
-    //周报完成的工作
-    private String risk;
-    //目前的风险
-    @TableField(value = "nextplan")
-    private String nextPlan;
-    //下周计划
-    private boolean delay;
-    //是否有延迟
->>>>>>> origin/main
 }
