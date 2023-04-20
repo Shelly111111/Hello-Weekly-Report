182a7c3390b274ea7ff3e836ed9ed30e1a2f66a6
戈 张 <921519274@qq.com>
2023-04-20 17:15:26 +0800

    更新打卡记录

--- spring-Java/src/main/java/com/hello/weekly/pojo/UserDailyCheckin.java
+++ spring-Java/src/main/java/com/hello/weekly/pojo/UserDailyCheckin.java
@@ -1,9 +1,12 @@
 package com.hello.weekly.pojo;
 
+import com.alibaba.fastjson.annotation.JSONField;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableName;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.*;
 
+import java.sql.Time;
 import java.util.Date;
 
 @AllArgsConstructor
@@ -13,14 +16,19 @@ import java.util.Date;
 @ToString
 @TableName("ClockInRecord")
 public class UserDailyCheckin {
+
+    @JsonIgnore
+    @JSONField(serialize = false)
     private Integer Id;
 
+    @JsonIgnore
+    @JSONField(serialize = false)
     @TableField(value = "userId")
     private Integer user_id;
 
     private Date date;
 
-    private String time;
+    private Time time;
 
     @TableField(value = "workHour")
     private Integer work_hour;
