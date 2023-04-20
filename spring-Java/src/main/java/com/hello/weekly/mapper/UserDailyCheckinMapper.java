777d92a1b3e1125c1d472f20a7b834335d649dbf
戈 张 <921519274@qq.com>
2023-04-20 17:11:14 +0800

    更新打卡记录

--- spring-Java/src/main/java/com/hello/weekly/mapper/UserDailyCheckinMapper.java
+++ spring-Java/src/main/java/com/hello/weekly/mapper/UserDailyCheckinMapper.java
@@ -3,14 +3,9 @@ package com.hello.weekly.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.hello.weekly.pojo.UserDailyCheckin;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Mapper
 @Repository
 public interface UserDailyCheckinMapper extends BaseMapper<UserDailyCheckin> {
-    @Select("select * from clockinrecord where userId = #{user_id}")
-    public List<UserDailyCheckin> getAllByUserId(Integer user_id);
 }
--- spring-Java/src/main/java/com/hello/weekly/service/UserDailyCheckinService.java
+++ spring-Java/src/main/java/com/hello/weekly/service/UserDailyCheckinService.java
@@ -4,8 +4,37 @@ import com.hello.weekly.pojo.UserDailyCheckin;
 
 import java.util.List;
 
+/**
+ * 打卡记录
+ *
+ * @author: 冯松
+ * @updateauthor: 漫舞枪神
+ * @updatedate: 2023/4/20
+ */
 public interface UserDailyCheckinService {
     // 根据用户ID和日期查询打卡记录
-    public List<UserDailyCheckin> getAllByUserId(Integer user_id);
+    List<UserDailyCheckin> getAllByUserId(Integer user_id);
 
+    /**
+     * 根据用户ID获取分页数据
+     * @param userId
+     * @param currentpage
+     * @param size
+     * @return
+     *
+     * @author: 漫舞枪神
+     * @date: 2023/4/20
+     */
+    List<UserDailyCheckin> getRecordByPage(Integer userId, Integer currentpage, Integer size);
+
+    /**
+     * 根据用户ID获取页面数量
+     * @param userId
+     * @param size
+     * @return
+     *
+     * @author: 漫舞枪神
+     * @date: 2023/4/20
+     */
+    Long getPageTotalSize(Integer userId, Integer size);
 }
